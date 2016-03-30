65c3957a67dcefb4e01413eca7cb90d3ec98ebae
qijingyu2013 <qijingyu2009@gmail.com>
2016-03-30 12:38:21 +0800

    重新上传

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -109,6 +109,7 @@ Route::group(['prefix' => 'admina/about', 'namespace' => 'Admina', 'middleware' 
 	Route::get('calender', 'CalenderController@getList');
 
 	//关于大家银 end
+
 });
 
 // Route::group(['prefix' => 'admina','namespace' => 'Admina','middleware' => 'auth'],function()
